a4ff94ac2d1ab7bc19d2352885f00aad08eab0ba
Muhammad Afdal Adiguna <m.afdal.adiguna@gmail.com>
2025-07-30 20:39:07 +0700

    sadsa

--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -29,11 +29,11 @@
                         @foreach ($invoices as $invoice)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
-                                <td>{{ $invoice['namaPelanggan'] }}</td>
-                                <td>{{ \Carbon\Carbon::parse($invoice['tanggal'])->format('d M Y') }}</td>
+                                <td>{{ $invoice->namaPelanggan }}</td>
+                                <td>{{ \Carbon\Carbon::parse($invoice->tanggal)->format('d M Y') }}</td>
                                 <td>
                                     <div class="invoice-card-actions">
-                                        <a href="{{ route('invoices.show', $invoice['id']) }}" class="btn btn-sm btn-primary">
+                                        <a href="{{ route('invoices.show', $invoice->id) }}" class="btn btn-sm btn-primary">
                                             <i class="fas fa-eye"></i> Lihat
                                         </a>
                                         <a href="{{ route('invoice.download.pdf', $invoice->id) }}"
